1fcb907e66bee7d17275f1a128f6c617f223c807
unknown <ishiroxz@yahoo.com>
2014-12-03 17:19:51 -0500

    ebay try

--- public/features/controllers.js
+++ public/features/controllers.js
@@ -8,14 +8,34 @@ ngIdpControllers.controller('SignInCtrl', ['$scope','DataFactory', function ($sc
 	$scope.validate = function () {
 		
 		DataFactory.checkPost("/AuthenticateUser", $scope.login)
-        		 	.success(function(json){
-        		 		console.log("success called" + json.name);
-        		 		location.replace("http://localhost:3000/" + "#homePage?name="+json.name);  
-        	});
+		.success(function(json){
+			console.log("success called" + json.name);
+			location.replace("http://localhost:3000/" + "#homePage?name="+json.name);  
+		});
 	}
 }]);
 
-ngIdpControllers.controller('HomepageCtrl', ['$scope', function ($scope, $http) {
+ngIdpControllers.controller('HomepageCtrl', ['$scope','DataFactory', function ($scope, $http) {
 	console.log("Hello from HomepageCtrl");
+	var filterarray = [
+	{"name":"MaxPrice", 
+	"value":"25", 
+	"paramName":"Currency", 
+	"paramValue":"USD"},
+	{"name":"FreeShippingOnly", 
+	"value":"true", 
+	"paramName":"", 
+	"paramValue":""},
+	{"name":"ListingType", 
+	"value":["AuctionWithBIN", "FixedPrice", "StoreInventory"], 
+	"paramName":"", 
+	"paramValue":""},
+	];
 
+	$scope.searchFromEbay = function () {
+		console.log("Hello from searchFromEbay");
+		console.log($scope.keyword);
+		console.log($scope.category);
+	}
+	
 }]);
